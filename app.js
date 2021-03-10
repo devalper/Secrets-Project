06fe93973f3a5950d93544b187b131f52928e614
Alper SAYICI <alpersayici@gmail.com>
2021-03-10 22:22:15 +0300

    Create database and set requests for login and register pages  with Username and Password Only

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // REQUIRING OUR DEPENDENCIES
 const express = require('express');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+
 
 // DECLARING OUR EXPRESS APP
 const app = express();
@@ -14,12 +16,79 @@ app.use(express.urlencoded({ extended: true }));
 // SETTING OUR STATIC FOLDER
 app.use(express.static('public'));
 
+//DATABASE CREATION STEPS WITH MONGOOSE
+
+// 1) CREATE NEW DATABASE INSIDE MongoDB
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+
+// 2) CREATE NEW SCHEMA
+const userSchema = new mongoose.Schema({
+
+  email: String,
+  password: String
+});
+
+// 3) CREATE NEW MODEL (our collection) BASED ON OUR SCHEMA
+
+const User = new mongoose.model('User', userSchema);
 
 
+
+
+// SEETING OUR GET ROUTES
+
+app.get('/', (req, res) => {
+  res.render("home");
+});
+
+app.get('/login', (req, res) => {
+  res.render("login");
+});
+
+app.get('/register', (req, res) => {
+  res.render("register");
+});
+
+// SETTING OUR POST ROUTES
+
+app.post('/register', (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username }, (err, foundUser) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        } else {
+          console.log("Please check your password and try again.");
+        }
+      } else {
+        console.log("User not found, please check your email and try again or register.");
+      }
+    }
+  });
+});
 
 // LISTEN TO PORT
 app.listen(3000, () => {
 
-    console.log('Server has started on port 3000');
-  
-  });
+  console.log('Server has started on port 3000');
+
+});
