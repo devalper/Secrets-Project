caebdee55bcd0f970b78aa905b1baaa396e6c71c
Alper SAYICI <alpersayici@gmail.com>
2021-03-11 22:27:15 +0300

    Add hashing with md5 -- Delete mongoose-encryption

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 
 // DECLARING OUR EXPRESS APP
@@ -30,9 +30,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-// 2-a) ADDING ENCRYPTION PLUGIN BEFORE CREATING OUR MODEL
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
-
 
 // 3) CREATE NEW MODEL (our collection) BASED ON OUR SCHEMA
 
@@ -58,7 +55,7 @@ app.get('/register', (req, res) => {
 app.post('/register', (req, res) => {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save((err) => {
     if (err) {
@@ -71,7 +68,7 @@ app.post('/register', (req, res) => {
 
 app.post('/login', (req, res) => {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({ email: username }, (err, foundUser) => {
     if (err) {
