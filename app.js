c6bb8d2644acc0a3ad7f96c51c7692f4b363dc4f
Alper SAYICI <alpersayici@gmail.com>
2021-03-12 18:27:01 +0300

    Add Salting and Hashing with bcrypt Instead of MD5

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 // DECLARING OUR EXPRESS APP
 const app = express();
@@ -51,35 +51,47 @@ app.get('/register', (req, res) => {
 });
 
 // SETTING OUR POST ROUTES
+// Instead of "MD5" we're using "bcrypt" for hashing
 
 app.post('/register', (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
+
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+
+    const newUser = new User({
+
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+
+    });
+
   });
-  newUser.save((err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  });
+
 });
 
 app.post('/login', (req, res) => {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({ email: username }, (err, foundUser) => {
     if (err) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        } else {
-          console.log("Please check your password and try again.");
-        }
+        bcrypt.compare(password, foundUser.password, (err, result) => {
+
+          if (result === true) {
+            res.render("secrets");
+          } else {
+            console.log("Please check your password and try again.");
+          }
+        });
       } else {
         console.log("User not found, please check your email and try again or register.");
       }
