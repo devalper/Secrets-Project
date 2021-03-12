3957c1afec95eea41cfe00f17de62a4d572db09f
Alper SAYICI <alpersayici@gmail.com>
2021-03-12 22:09:27 +0300

    Use passport, passport-local, passport-local-mongoose, express-session

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
 
 // DECLARING OUR EXPRESS APP
 const app = express();
@@ -18,10 +20,22 @@ app.use(express.urlencoded({ extended: true }));
 // SETTING OUR STATIC FOLDER
 app.use(express.static('public'));
 
+// SETTING express-session
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+// INITIALIZING passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 //DATABASE CREATION STEPS WITH MONGOOSE
 
 // 1) CREATE NEW DATABASE INSIDE MongoDB
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+mongoose.set('useCreateIndex', true); // for fixing the deprecation warning
 
 // 2) CREATE NEW SCHEMA
 const userSchema = new mongoose.Schema({
@@ -30,11 +44,18 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
+// SET UP passport-local-mongoose
+userSchema.plugin(passportLocalMongoose);
 
 // 3) CREATE NEW MODEL (our collection) BASED ON OUR SCHEMA
 
 const User = new mongoose.model('User', userSchema);
 
+// SIMPLIFIED passport/passport-local CONFIGURATION
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // SETTING OUR GET ROUTES
 
@@ -50,53 +71,61 @@ app.get('/register', (req, res) => {
   res.render("register");
 });
 
+app.get('/secrets', (req, res) => {
+
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect('/login');
+  }
+});
+
+app.get('/logout', (req, res) => {
+
+  req.logout();
+  res.redirect('/');
+});
+
 // SETTING OUR POST ROUTES
-// Instead of "MD5" we're using "bcrypt" for hashing
+// USING PASSPORT FOR SALTING-HASHING(passport does it automatically), AUTHENTICATION AND COOKIES
 
 app.post('/register', (req, res) => {
 
-  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+  User.register({ username: req.body.username }, req.body.password, (err, user) => {
 
-    const newUser = new User({
-
-      email: req.body.username,
-      password: hash
-    });
-    newUser.save((err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-
-    });
+    if (err) {
+      console.log(err);
+      res.redirect('/register');
+    } else {
+      passport.authenticate("local")(req, res, () => {
+        res.redirect('/secrets');
+      });
+    }
 
   });
 
 });
 
 app.post('/login', (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
 
-  User.findOne({ email: username }, (err, foundUser) => {
+  const user = new User({
+
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, (err) => {
+
     if (err) {
       console.log(err);
     } else {
-      if (foundUser) {
-        bcrypt.compare(password, foundUser.password, (err, result) => {
-
-          if (result === true) {
-            res.render("secrets");
-          } else {
-            console.log("Please check your password and try again.");
-          }
-        });
-      } else {
-        console.log("User not found, please check your email and try again or register.");
-      }
+      passport.authenticate("local")(req, res, () => {
+        res.redirect('/secrets');
+      });
     }
+
   });
+
 });
 
 
