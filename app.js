57113cc13563e23ebf7c2150bee86e62657019ac
Alper SAYICI <alpersayici@gmail.com>
2021-03-14 17:33:59 +0300

    Project End - Allow users to submit secrets

--- app.js
+++ app.js
@@ -45,7 +45,8 @@ const userSchema = new mongoose.Schema({
 
   email: String,
   password: String,
-  googleId: String
+  googleId: String,
+  secret: String
 });
 
 // SET UP passport-local-mongoose
@@ -117,8 +118,21 @@ app.get('/register', (req, res) => {
 
 app.get('/secrets', (req, res) => {
 
+  User.find({ "secret": { $ne: null } }, (err, foundUsers) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render("secrets", { usersWithSecrets: foundUsers });
+      }
+    }
+  });
+});
+
+app.get('/submit', (req, res) => {
+
   if (req.isAuthenticated()) {
-    res.render("secrets");
+    res.render("submit");
   } else {
     res.redirect('/login');
   }
@@ -170,6 +184,25 @@ app.post('/login', (req, res) => {
 
   });
 
+});
+
+app.post('/submit', (req, res) => {
+  const submittedSecret = req.body.secret;
+
+  console.log(req.user.id);
+
+  User.findById(req.user.id, (err, foundUser) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(() => {
+          res.redirect('/secrets');
+        });
+      }
+    }
+  });
 });
 
 
